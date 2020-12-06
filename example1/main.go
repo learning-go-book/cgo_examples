5c32d8e190de21dcc8b243316c80e31bba0cae6a
Jon Bodner <jon@bodnerfamily.com>
2020-12-05 23:18:16 -0500

    add to example1

--- example1/main.go
+++ example1/main.go
@@ -5,6 +5,7 @@ import "fmt"
 /*
 	#include <stdio.h>
 	#include <math.h>
+	#include "mylib.h"
 
 	int add(int a, int b) {
 		int sum = a + b;
@@ -18,4 +19,5 @@ func main() {
 	sum := C.add(3, 2)
 	fmt.Println(sum)
 	fmt.Println(C.sqrt(100))
+	fmt.Println(C.multiply(10, 20))
 }
